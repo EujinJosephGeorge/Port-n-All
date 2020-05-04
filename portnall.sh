b01d8f7572f74f6876124676c00b32cced83ee4d
Ug1N <41055202+EujinJosephGeorge@users.noreply.github.com>
2020-05-04 09:12:38 +0000

    Update portnall.sh

--- portnall.sh
+++ portnall.sh
@@ -4,7 +4,7 @@ echo -e "\e[1;32mYou Forgot the IP Address\e[0m"
 echo -e "\e[1;32mSyntax: sudo ./portnall.sh\e[0m"
 exit
 else
-nmap -T4 -p- $1 | grep "/" | cut -d " " -f 1 | tr -d "/tcpud" > port.txt #Runs nmap to scan all ports. It greps for line with '/' and uses delimiter to remove spaces and selects the 1st field and then truncates the characters and wites the output to port.txt
+nmap -T4 --min-rate=1000 -p- $1 | grep "/" | cut -d " " -f 1 | tr -d "/tcpud" > port.txt #Runs nmap to scan all ports. It greps for line with '/' and uses delimiter to remove spaces and selects the 1st field and then truncates the characters and wites the output to port.txt
 sed '1d' port.txt > ports.txt #Uses sed to remove 1st line as it was unnecessary characters
 tr '\n' ',' < ports.txt > portnall.txt #Uses truncate to add comma so that the port number wont be in different line as it will rerun the code for each port
 echo
